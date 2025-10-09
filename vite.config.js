c6bacd9ba9a7db3d4a9e85da12beb613372cb1bf
muriloviniciustpf <muriloviniciustpf@gmail.com>
2025-10-08 22:17:09 -0300

    Configura build para GitHub Pages e gera arquivos

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'path' // Importe o módulo 'path' do Node.js
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ProUni_Front/", // Substitua "ProUni_Front" pelo nome do seu repositório no GitHub
+  build: {
+    outDir: 'docs' // Altera a pasta de saída de 'dist' para 'docs'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
